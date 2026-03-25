345f4c08da4ba278356a60dffa4ae09e903b4c86
Theo-panella <panellatheo@gmail.com>
2026-03-25 14:26:11 -0300

    Watchdog implementado e funcionando - terminar a pré filtragem(bem ruim ainda)

--- nerdy_web/MiniMim/MiniMim.py
+++ nerdy_web/MiniMim/MiniMim.py
@@ -1,19 +1,42 @@
+import time
 import re
 import yaml
 
-# Abre arquivo de configuração do pre-filtro
-with open ('config.yaml', 'r' ) as file:
-    data = yaml.safe_load(file)
+from watchdog.events import FileSystemEvent, FileSystemEventHandler,FileModifiedEvent
+from watchdog.observers import Observer
 
-# Se for Flask pega os paramentros dados e aplica compile para aumentar o desempenho
+services_list = ['Flask','Nginx']
 
-regex = {}
+with open('nerdy_web/MiniMim/config.yaml', 'r') as config_file:
+    config = yaml.safe_load(config_file)
+    aceito = re.compile(".* nerdy-web flask[.*]: Accepted password for .*")
+    negado = re.compile(".* nerdy-web flask[.*]: Failed password for .*")
 
-for service, config in data.items():
-    regex[service] = {
-        'failure': re.compile(config.get('Failure', '')),
-        'accepted': re.compile(config.get('Accepted', ''))
-    }
+def pre_filtragem(aceito,negado,arquivo):
+    log_accepted = re.findall(aceito,arquivo)
+    log_failure = re.findall(negado,arquivo)
+    print(log_accepted, arquivo)
+    print(log_failure, arquivo)
     
-    print(regex['Flask'])
-    
+
+class MyEventHandler(FileSystemEventHandler):
+    def on_any_event(self, event: FileSystemEvent) -> None:
+        #print(f"Evento: {event.event_type} / caminho: {event.src_path} / Destino: {event.dest_path} / {event.is_synthetic}")
+        #print(event)
+        if event.src_path == ".\\flask_logs.txt" and event.event_type == "modified":
+            print("Nova tentativa de Login detectada")
+            print("Analisando log...")
+            pre_filtragem(aceito,negado,"/flask_logs.txt")
+            
+
+
+event_handler = MyEventHandler()
+observer = Observer()
+observer.schedule(event_handler, ".", recursive=True)
+observer.start()
+try:    
+    while True:
+        time.sleep(2)
+finally:
+    observer.stop()
+    observer.join()
